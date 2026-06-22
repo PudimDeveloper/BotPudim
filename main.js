fffa8134ad980d27a941983ca105a17b88bbf8b3
PudimDeveloper <jonnas9908@gmail.com>
2026-06-21 22:39:43 -0300

    NEW FEATURES

---
+++ main.js
@@ -0,0 +1,37 @@
+const mineflayer = require("mineflayer")
+const express = require("express")
+
+const app = express()
+
+function criarBot() {
+    const bot = mineflayer.createBot({
+        host: "eradocobre.aternos.me",
+        port: 61001,
+        username: "PudimBot"
+    })
+
+    bot.on("spawn", () => {
+        console.log("Conectado")
+    })
+
+    bot.on("end", () => {
+        console.log("Desconectado. Reconectando em 5s...")
+
+        setTimeout(criarBot, 1000)
+    })
+
+    bot.on("error", err => {
+        console.log(err)
+    })
+    return bot
+}
+
+criarBot()
+
+app.get("/", (req, res) => {
+    res.send("PudimBot online")
+})
+
+app.listen(3000, () => {
+    console.log("Web server iniciado")
+})
